541388108d66d9a448bce2e7161dea3c29da1521
Francois D'Agostini <francois.dagostini@gmail.com>
2017-08-06 00:58:52 +0200

    main page working

---
+++ src/Book.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+
+class Book extends Component {
+    render() {
+
+        const {book, onMoveBook} = this.props
+
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select
+                            defaultValue={book.shelf}
+                            onChange={e => {onMoveBook(book, e.target.value)}}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading" >Currently Reading</option>
+                            <option value="wantToRead" >Want to Read</option>
+                            <option value="read" >Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                {book.authors.map(author => (
+                    <div key={author} className="book-authors">{author}</div>
+                ))}
+
+            </div>
+        )
+    }
+}
+
+export default Book
+
+
---
+++ src/MainPage.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react'
+import {Link} from "react-router-dom";
+import Book from "./Book";
+
+class MainPage extends Component {
+
+
+
+
+    render() {
+
+        const {books, onMoveBook} = this.props
+
+        const currentlyReading = books.filter(book => book.shelf === "currentlyReading");
+        const wantToRead = books.filter(book => book.shelf === "wantToRead");
+        const read = books.filter(book => book.shelf === "read");
+
+        console.log("currentlyReading",currentlyReading);
+        console.log("wanttoread",wantToRead);
+        console.log("read",read);
+
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {currentlyReading.map(book => (
+                                        <li key={book.id}><Book book={book} onMoveBook={onMoveBook}/></li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {wantToRead.map(book => (
+                                        <li key={book.id}><Book book={book} onMoveBook={onMoveBook}/></li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {read.map(book => (
+                                        <li key={book.id}><Book book={book} onMoveBook={onMoveBook}/></li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to={process.env.PUBLIC_URL + "/search"}>Add a book</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MainPage
